10832deb0073bd5a90c238109352f7cb2ea97152
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-07-01 17:44:28 +0530

    Time: 66 ms (86.45%), Space: 19.7 MB (68.61%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,33 +2,42 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        queue<pair<pair<int, int>, int>> q;
-        // { {r, c}, len}
-        if(grid[0][0]) return -1;
-        q.push({{0, 0}, 1});
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-        vis[0][0] = 1;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        grid[0][0] = 1;
+
+        int len = 1;
+        vector<int> dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        vector<int> dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+
         while (!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int len = q.front().second;
-            q.pop();
-            if (row == n - 1 && col == n - 1) {
-                return len;
-            }
-            vector<int> dr = {-1, -1, -1, 0, 0, 1, 1, 1};
-            vector<int> dc = {-1, 0, 1, -1, 1, -1, 0, 1};
-            for (int i = 0; i < 8; i++) {
-                int newRow = row + dr[i];
-                int newCol = col + dc[i];
-                if (newRow >= 0 && newRow < n && newCol >= 0 &&
-                     newCol < n && !vis[newRow][newCol]
-                         && !grid[newRow][newCol]) {
-                    q.push({{newRow, newCol}, len + 1});
-                    vis[newRow][newCol] = 1;
+            int size = q.size();
+            while (size--) {
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+
+                if (row == n - 1 && col == n - 1) {
+                    return len;
+                }
+
+                for (int i = 0; i < 8; i++) {
+                    int newRow = row + dr[i];
+                    int newCol = col + dc[i];
+                    if (newRow >= 0 && newRow < n && newCol >= 0 &&
+                        newCol < n && grid[newRow][newCol] == 0) {
+                        q.push({newRow, newCol});
+                        grid[newRow][newCol] = 1;
+                    }
                 }
             }
+            len++;
         }
+
         return -1;
     }
 };
