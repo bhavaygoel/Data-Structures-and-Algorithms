0105502b14d193c5776dc730e0d6ab32398253c9
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-07-04 15:18:51 +0530

    Time: 34 ms (22.85%), Space: 15.3 MB (21.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,52 +1,26 @@
-class DisjointSet{
-public:
-    vector<int> parent, size;
-    DisjointSet(int n){
-        parent.resize(n);
-        size.resize(n);
-        for(int i=0; i<n; i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findParent(int node){
-        if(node == parent[node]) return node;
-
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void unionS(int u, int v){
-        int up = findParent(u);
-        int vp = findParent(v);
-        if(up == vp) return;
-        if(size[up] > size[vp]){
-            parent[vp] = up;
-            size[up] += size[vp];
-        }
-        else{
-            parent[up] = vp;
-            size[vp] += size[up];
-        }
-    }
-};
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size();
-        DisjointSet ds(V);
-        for(int i=0; i<V; i++){
-            for(int j=0; j<V; j++){
-                if(isConnected[i][j] == 1){
-                ds.unionS(i, j);
-                }
+    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visit) {
+        visit[node] = true;
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (isConnected[node][i] && !visit[i]) {
+                dfs(i, isConnected, visit);
             }
         }
-        int cnt = 0;
-        for(int i=0; i<V; i++){
-            if(ds.parent[i] == i) cnt++;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int numberOfComponents = 0;
+        vector<bool> visit(n);
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                dfs(i, isConnected, visit);
+            }
         }
-        return cnt;
+
+        return numberOfComponents;
     }
 };
