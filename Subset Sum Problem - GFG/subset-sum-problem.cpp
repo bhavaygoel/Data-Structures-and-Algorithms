507bf3be4d5a63174c86f6f33abc519a926be5b5
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-07-11 21:04:59 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -25,17 +25,18 @@ public:
         // code here 
         vector<vector<int>> t(arr.size()+1, vector<int>(sum+1, -1));
         int n = arr.size();
-        return helper(arr, n, sum, t);
-        // for(int j=0; j<sum+1; j++) t[0][j] = 0;
-        // for(int i=0; i< n+1; i++) t[i][0] = 1;
-        // for(int i=1; i< n+1; i++){
-        //     for(int j=1; j<sum+1; j++){
-        //         if(arr[i-1] <= j){
-        //             t[i-1][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-        //         }
-        //         else t[i-1][j] = t
-        //     }
-        // }
+        // return helper(arr, n, sum, t);
+        for(int j=0; j<sum+1; j++) t[0][j] = 0;
+        for(int i=0; i< n+1; i++) t[i][0] = 1;
+        for(int i=1; i< n+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(arr[i-1] <= j){
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+                else t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][sum];
     }
 };
 
