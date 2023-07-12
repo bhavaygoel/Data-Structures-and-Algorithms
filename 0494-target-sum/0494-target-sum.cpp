4443906b392cf96a3d9a2f32aea77669809a1c0f
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-07-12 19:39:28 +0530

    Time: 24 ms (54.67%), Space: 12.1 MB (44.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int target, int curr, int index, vector<vector<int>>& t, int total){
-        if(index >= nums.size()) {
-            if(curr == target){
-                return 1;
-            }
-            return 0;
-        }
-        if(t[index][total + curr] != -1){
-            return t[index][total + curr];
-        }
-        int val = nums[index];
-       t[index][total+curr] =  helper(nums, target, curr+val, index+1, t, total)+ helper(nums, target, curr-val, index+1, t, total);
-        return t[index][total+curr];
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int total=0;
+        int sum=0;
         for(auto& it: nums){
-            total += it;
+            sum += it;
         }
-        vector<vector<int>> t(nums.size()+1, vector<int>(2*total+1, -1));
-        return helper(nums, target, 0, 0, t, total);
+        if((sum-target)%2 != 0 || target > sum) return 0;
 
+        int n = nums.size();
+        int s2 = (sum - target) / 2;
+        vector<vector<int>> t(n+1, vector<int>(s2+1,0));
+        t[0][0] = 1;
+        for(int i=1; i<n+1; i++){
+            for(int j=0; j<s2+1; j++){
+                if(nums[i-1] <= j){
+                    t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j];
+                }
+                else t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][s2];
     }
 };
