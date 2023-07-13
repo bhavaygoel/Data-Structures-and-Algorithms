17c1a35ddf931f34ec0a1ec277fa6822003c405b
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-07-13 14:57:38 +0530

    Time: 11 ms (87.69%), Space: 19 MB (17.27%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<vector<int>> t(coins.size()+1, vector<int>(amount+1, 0));
-        t[0][0] = 1;
-        for(int i=1; i< coins.size()+1; i++){
-            for(int j=0; j<amount +1; j++){
-                if(coins[i-1] <= j){
-                    t[i][j] = t[i-1][j] + t[i][j-coins[i-1]];
-                }
-                else t[i][j] = t[i-1][j];
-            }
+    int helper(int index, int amount, vector<int>& coins, vector<vector<int>>& t){
+        if(index < 0){
+            return 0;
         }
-        return t[coins.size()][amount];
+        if(amount == 0) return 1;
+        if(t[index][amount] != -1) return t[index][amount];
+        if(coins[index] > amount){
+            t[index][amount] = helper(index-1, amount, coins, t);
+        }
+        else {
+            t[index][amount] = helper(index-1, amount, coins, t) + 
+                                helper(index, amount-coins[index], coins, t);
+        }
+        return t[index][amount];
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> t(coins.size()+1, vector<int>(amount + 1, -1));
+        return helper(coins.size()-1, amount, coins, t);
     }
 };
