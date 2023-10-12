7e1c7ce179bb19367a6b9932c5d2f5d43f682d70
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2023-10-12 19:22:54 +0530

    Time: 8 ms (79.56%), Space: 22.8 MB (39.72%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -18,13 +18,11 @@ public:
         vector<double> ans;
         while(!q.empty()){
             int size = q.size();
-            vector<int> level;
             double sum=0;
             for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
                 sum += node->val;
                 q.pop();
-                level.push_back(node->val);
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
